a12347880f6eded4de82bcd07d34ab98a44af355
Whims-Dev <110363728+Whims-Dev@users.noreply.github.com>
2023-02-11 20:22:28 -0800

    Update Unnamed Deepwoken ESP Edit.lua

--- scripts/Unnamed Deepwoken ESP Edit.lua
+++ scripts/Unnamed Deepwoken ESP Edit.lua
@@ -77,86 +77,7 @@ end
 local function Set(t, i, v) t[i] = v end
 
 local Teams = {};
-local CustomTeams = { -- Games that don't use roblox's team system
-	[2563455047] = {
-		Initialize = function()
-			Teams.Sheriffs = {}; -- prevent big error
-			Teams.Bandits = {}; -- prevent big error
-			local Func = game:GetService'ReplicatedStorage':WaitForChild('RogueFunc', 1);
-			local Event = game:GetService'ReplicatedStorage':WaitForChild('RogueEvent', 1);
-			local S, B = Func:InvokeServer'AllTeamData';
-
-			Teams.Sheriffs = S;
-			Teams.Bandits = B;
-
-			Event.OnClientEvent:Connect(function(id, PlayerName, Team, Remove) -- stolen straight from decompiled src lul
-				if id == 'UpdateTeam' then
-					local TeamTable, NotTeamTable
-					if Team == 'Bandits' then
-						TeamTable = TDM.Bandits
-						NotTeamTable = TDM.Sheriffs
-					else
-						TeamTable = TDM.Sheriffs
-						NotTeamTable = TDM.Bandits
-					end
-					if Remove then
-						TeamTable[PlayerName] = nil
-					else
-						TeamTable[PlayerName] = true
-						NotTeamTable[PlayerName] = nil
-					end
-					if PlayerName == LocalPlayer.Name then
-						TDM.Friendlys = TeamTable
-						TDM.Enemies = NotTeamTable
-					end
-				end
-			end)
-		end;
-		CheckTeam = function(Player)
-			local LocalTeam = Teams.Sheriffs[LocalPlayer.Name] and Teams.Sheriffs or Teams.Bandits;
-			
-			return LocalTeam[Player.Name] and true or false;
-		end;
-	};
-	[5208655184] = {
-		CheckTeam = function(Player)
-			local LocalLastName = LocalPlayer:GetAttribute'LastName' if not LocalLastName or IsStringEmpty(LocalLastName) then return true end
-			local PlayerLastName = Player:GetAttribute'LastName' if not PlayerLastName then return false end
-
-			return PlayerLastName == LocalLastName
-		end
-	};
-	[3541987450] = {
-		CheckTeam = function(Player)
-			local LocalStats = LocalPlayer:FindFirstChild'leaderstats';
-			local LocalLastName = LocalStats and LocalStats:FindFirstChild'LastName'; if not LocalLastName or IsStringEmpty(LocalLastName.Value) then return true; end
-			local PlayerStats = Player:FindFirstChild'leaderstats';
-			local PlayerLastName = PlayerStats and PlayerStats:FindFirstChild'LastName'; if not PlayerLastName then return false; end
-
-			return PlayerLastName.Value == LocalLastName.Value;
-		end;
-	};
-    [6032399813] = {
-		CheckTeam = function(Player)
-			local LocalStats = LocalPlayer:FindFirstChild'leaderstats';
-			local LocalGuildName = LocalStats and LocalStats:FindFirstChild'Guild'; if not LocalGuildName or IsStringEmpty(LocalGuildName.Value) then return true; end
-			local PlayerStats = Player:FindFirstChild'leaderstats';
-			local PlayerGuildName = PlayerStats and PlayerStats:FindFirstChild'Guild'; if not PlayerGuildName then return false; end
-
-			return PlayerGuildName.Value == LocalGuildName.Value;
-		end;
-	};
-    [5735553160] = {
-		CheckTeam = function(Player)
-			local LocalStats = LocalPlayer:FindFirstChild'leaderstats';
-			local LocalGuildName = LocalStats and LocalStats:FindFirstChild'Guild'; if not LocalGuildName or IsStringEmpty(LocalGuildName.Value) then return true; end
-			local PlayerStats = Player:FindFirstChild'leaderstats';
-			local PlayerGuildName = PlayerStats and PlayerStats:FindFirstChild'Guild'; if not PlayerGuildName then return false; end
-
-			return PlayerGuildName.Value == LocalGuildName.Value;
-		end;
-	};
-};
+local CustomTeams = {};
 
 local RenderList = {Instances = {}};
 
@@ -225,6 +146,7 @@ local DeepwokenInfo = {
 local Modules = {
 	[6032399813] = DeepwokenInfo, -- Etrean
     [6473861193] = DeepwokenInfo, -- Eastern Luminent
+	[8668476218] = DeepwokenInfo, -- Trial of One
     [5735553160] = DeepwokenInfo -- Depths
 };
 
