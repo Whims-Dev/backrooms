6791ae14e34c9d9a99c8b0ceb9488fa0e133e464
Whims-Dev <110363728+Whims-Dev@users.noreply.github.com>
2023-03-20 16:39:06 -0700

    Update VelocityBasedFly.lua

--- scripts/VelocityBasedFly.lua
+++ scripts/VelocityBasedFly.lua
@@ -61,7 +61,7 @@ shared.flyinput = UserInputService.InputBegan:Connect(function(input, gpe)
                 local Torso = Character:FindFirstChild("HumanoidRootPart");
                 if (Torso == nil) then return end
                 local Humanoid = Character:FindFirstChildWhichIsA("Humanoid");
-                if (Humanoid ~= nil) and (Humanoid.PlatformStand) then
+                if (Humanoid ~= nil) and (Humanoid.PlatformStand) and (game.GameId == 1359573625) then
                     keypress(0x30)
                     keyrelease(0x30)
                 end
@@ -72,9 +72,9 @@ shared.flyinput = UserInputService.InputBegan:Connect(function(input, gpe)
                     end
                 end
                 if (Control.L + Control.R ~= 0) or (Control.F + Control.B ~= 0) then
-                    FlySpeed = math.min(FlySpeed + 0.5 + (FlySpeed / MaxFlySpeed), MaxFlySpeed)
+                    FlySpeed = MaxFlySpeed
                 elseif (((Control.L + Control.R == 0) or (Control.F + Control.B == 0)) and FlySpeed ~= 0) then
-                    FlySpeed = math.max(FlySpeed - 5, 0);
+                    FlySpeed = 0
                 end
                 local DirectionCF = Camera.CFrame;
                 if ((Control.L + Control.R) ~= 0) or ((Control.F + Control.B) ~= 0) then
